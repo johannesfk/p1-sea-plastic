ebfe60c2c683573bbe4c4ea2f539d6998c813d46
BlonkoTron <pooplypoopinton@gmail.com>
2023-11-14 10:07:19 +0100

    bingus

--- Assets/Scripts/PollutionController.cs
+++ Assets/Scripts/PollutionController.cs
@@ -25,36 +25,43 @@ public class PollutionController : MonoBehaviour
     public float r1Waste = 10;
     public float r1Recycle = 1;
     public float r1TrashDestroyed = 1;
+    public float r1pollutionProcent;
 
     [Header("Region2")]
     [SerializeField] private float region2Polution;
     public float r2Waste = 10;
     public float r2Recycle = 1;
     public float r2TrashDestroyed = 1;
+    public float r2pollutionProcent;
 
     [Header("Region3")]
     [SerializeField] private float region3Polution;
     public float r3Waste = 10;
     public float r3Recycle = 1;
     public float r3TrashDestroyed = 1;
+    public float r3pollutionProcent;
 
     [Header("Region4")]
     [SerializeField] private float region4Polution;
     public float r4Waste = 10;
     public float r4Recycle = 1;
     public float r4TrashDestroyed = 1;
+    public float r4pollutionProcent;
 
     [Header("Region5")]
     [SerializeField] private float region5Polution;
     public float r5Waste = 10;
     public float r5Recycle = 1;
     public float r5TrashDestroyed = 1;
+    public float r5pollutionProcent;
 
     [Header("Region6")]
     [SerializeField] private float region6Polution;
     public float r6Waste = 5;
     public float r6Recycle = 1;
     public float r6TrashDestroyed = 1;
+    public float r6pollutionProcent;
+
 
     void Awake()
     {
@@ -65,13 +72,24 @@ public class PollutionController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-
+        //the regions polution calculations
         region1Polution = r1Waste - r1Recycle - r1TrashDestroyed;
         region2Polution = r2Waste - r2Recycle - r2TrashDestroyed;
         region3Polution = r3Waste - r3Recycle - r3TrashDestroyed;
         region4Polution = r4Waste - r4Recycle - r4TrashDestroyed;
         region5Polution = r5Waste - r5Recycle - r5TrashDestroyed;
         region6Polution = r6Waste - r6Recycle - r6TrashDestroyed;
+
+        //
+        r1pollutionProcent = region1Polution / r1Waste * 100;
+        r2pollutionProcent = region2Polution / r2Waste * 100;
+        r3pollutionProcent = region3Polution / r3Waste * 100;
+        r4pollutionProcent = region4Polution / r4Waste * 100;
+        r5pollutionProcent = region5Polution / r5Waste * 100;
+        r6pollutionProcent = region6Polution / r6Waste * 100;
+
+
+
 
         polutionMultiplier = region1Polution + region2Polution + region3Polution + region4Polution + region5Polution + region6Polution;
 
