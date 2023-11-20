623940234c1e53179b8daa590c4d7a46cdb5d5a6
BlonkoTron <pooplypoopinton@gmail.com>
2023-11-20 10:35:07 +0100

    REGION WORKS

--- Assets/Scripts/PollutionController.cs
+++ Assets/Scripts/PollutionController.cs
@@ -4,40 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.UIElements;
+using Unity.Mathematics;
 
-public class WorldStats
-{
-    public static List<float> regionPolution;
-
-    public static float polutionTotal;
-
-  //  public static object polutionRegion { get; internal set; }
-    
-    public void Awake()
-    {
-        polutionTotal = 0;
-        Debug.Log("finkledink");
-    }
-
-    private void Update()
-    {
-
-        if (regionPolution != null)
-        {
-            foreach (var item in regionPolution)
-            {
-                polutionTotal += item;
-            }
-        }
-        
-    }
-
-}
 
 [System.Serializable]
 public class Region
 {
 
+
+    public int regionNumber;
     [Header("Amounts")]
     public float regionPolution;
     public float regionWaste;
@@ -49,24 +24,6 @@ public class Region
     public float regionRecyclePercentage;
     public float regionTrashDestroyedPercentage;
     public float regionLandfillPercentage;
-
-    public void Update()
-    {
-        regionPolution = regionWaste - regionRecycle - regionTrashDestroyed - regionLandfilled;
-
-        Debug.Log("dillermand");
-
-        //region Percentages
-        regionPollutionPercentage = regionPolution / regionWaste * 100;
-
-        regionRecyclePercentage = regionRecycle / regionWaste * 100;
-
-        regionTrashDestroyedPercentage = regionTrashDestroyed / regionWaste * 100;
-
-        regionLandfillPercentage = regionLandfilled / regionWaste * 100;
-
-
-    }
 }
 
 
@@ -76,16 +33,19 @@ public class PollutionController : MonoBehaviour
 
     public List<Region> regions;
 
+    public int currentRegion;
+
     [Header("Stuff")]
     public UnityEngine.UI.Slider slider;
 
     [Header("World Stats")]
+    public float totalRegionPolution;
     public float polutionPercentage;
 
-    [SerializeField] private TMP_Text polutionProcentText;
+    [SerializeField] private TMP_Text polutionPercentageText;
+    [SerializeField] private TMP_Text landfillPercentageText;
 
     public float polution = 0;
-    public float polutionMultiplier;
 
     private float startPolution = 200;
     private float polutionThreshhold = 10000;
@@ -95,46 +55,53 @@ public class PollutionController : MonoBehaviour
         polution = startPolution;
         instance = this;
 
-        if (WorldStats.regionPolution != null)
+        currentRegion = 0;
+
+        if (regions.Count > 0)
         {
-            if (WorldStats.regionPolution.Count > 0)
+            for (int i = 0;i < regions.Count; i++)
             {
-                for (int i = 0; i < regions.Count; i++)
-                {
-                    WorldStats.regionPolution[i] = regions[i].regionPolution;
-                }
+                regions[i].regionNumber = i;
             }
         }
-       
+
     }
 
     // Update is called once per frame
     private void Update()
     {
 
-        polutionMultiplier = WorldStats.polutionTotal;
-
         polutionPercentage = polution / polutionThreshhold * 100;
+
+        //region percentages
+        regions[currentRegion].regionPollutionPercentage = regions[currentRegion].regionPolution / regions[currentRegion].regionWaste * 100;
+        regions[currentRegion].regionRecyclePercentage = regions[currentRegion].regionRecycle / regions[currentRegion].regionWaste * 100;
+        regions[currentRegion].regionTrashDestroyedPercentage = regions[currentRegion].regionTrashDestroyed / regions[currentRegion].regionWaste * 100;
+        regions[currentRegion].regionLandfillPercentage = regions[currentRegion].regionLandfilled / regions[currentRegion].regionWaste * 100;
 
         slider.value = polutionPercentage;
         
-        polutionProcentText.text = ((int)polutionPercentage + "%").ToString();
+
+
+        landfillPercentageText.text = regions[currentRegion].regionLandfillPercentage + "%";
+
+        polutionPercentageText.text = ((int)polutionPercentage + "%").ToString();
+
+
 
     }
-
-    private void FixedUpdate()
+    
+    public void EndDayAdd()
     {
 
-        if (polution >= polutionThreshhold)
+        if (regions.Count > 0)
         {
-            Debug.Log("Du fucking Tabte lol");
+            for (int i = 0; i < regions.Count; i++)
+            {
+                polution =+ regions[i].regionPolution;
+            }
         }
-
-        if (polution < polutionThreshhold)
-        {
-            polution += Time.fixedDeltaTime * polutionMultiplier;
-        }
-
     }
+
 
 }
