71430b59ad332a835368f23d01f58727e01786a5
BlonkoTron <133660863+BlonkoTron@users.noreply.github.com>
2023-12-11 09:17:12 +0100

    Update UIClickThroughFix.cs
    did actually not work

--- Assets/Scripts/UIClickThroughFix.cs
+++ Assets/Scripts/UIClickThroughFix.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class UIClickThroughFix : MonoBehaviour
 {
-
     public static bool uiActive;
+
+    private void Awake()
+    {
+        uiActive = false;
+    }
 
     void OnEnable()
     {
