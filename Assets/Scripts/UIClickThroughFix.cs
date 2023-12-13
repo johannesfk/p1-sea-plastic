79b3d5c8a0e9e497f958442e5c44610c5e976d16
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2023-12-13 11:13:35 +0100

    Update UIClickThroughFix.cs

--- Assets/Scripts/UIClickThroughFix.cs
+++ Assets/Scripts/UIClickThroughFix.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIClickThroughFix : MonoBehaviour
-{    
+{
     void OnEnable()
     {
-       HexGrid.instance.uiActive = true;
+        HexInteraction.instance.uiActive = true;
     }
 
     // Update is called once per frame
     void OnDisable()
     {
-        HexGrid.instance.uiActive = false;
+        HexInteraction.instance.uiActive = false;
     }
 }
