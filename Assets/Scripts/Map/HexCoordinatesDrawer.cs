5fe59502e03cdc1e4dd828048ccc504047d96ced
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2023-12-14 15:24:38 +0100

    Settings

--- Assets/Scripts/Map/HexCoordinatesDrawer.cs
+++ Assets/Scripts/Map/HexCoordinatesDrawer.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/* using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -17,4 +17,4 @@ public class HexCoordinatesDrawer : PropertyDrawer
         position = EditorGUI.PrefixLabel(position, label);
         GUI.Label(position, coordinates.ToString());
     }
-}
+} */
